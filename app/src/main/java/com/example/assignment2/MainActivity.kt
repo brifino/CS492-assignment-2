98196c9164cef89053748e93f33e8d5c286dfa13
Brahm Rifino <rifinob@oregonstate.edu>
2023-07-06 03:48:57 -0700

    Cleaned some stuff up. Works great

--- app/src/main/java/com/example/assignment2/MainActivity.kt
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -1,3 +1,13 @@
+/**
+ *  Assignment 2 / Simple Interactive App
+ *
+ *  Brahm Rifino / rifinob@oregonstate.edu
+ *  CS492 / Oregon State University
+ */
+
+
+
+
 package com.example.assignment2
 
 import android.os.Bundle
@@ -23,9 +33,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+
 import com.example.assignment2.ui.theme.Assignment2Theme
 
 class MainActivity : ComponentActivity() {
@@ -36,7 +55,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.White
                 ) {
                     Counter()
                 }
@@ -56,28 +75,53 @@ fun Counter(modifier: Modifier = Modifier) {
 
 @Composable
 fun HandleCount(modifier: Modifier = Modifier) {
-    var count by remember { mutableStateOf(1) }
+    var count by remember { mutableStateOf(3) }
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Spacer(modifier = Modifier.height(500.dp))
-        Row(horizontalArrangement = Arrangement.SpaceAround) {
+        Spacer(modifier = Modifier.height(75.dp))
+        Image(
+            painter = painterResource(id = R.drawable.dissc),
+            contentDescription = "App Logo",
+            modifier = Modifier.size(325.dp)
+                //.weight(1f)
+                .shadow(5.dp)
+                .border(10.dp, Color.White)
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+        Text("$count", fontSize = (36.sp))
+        Spacer(modifier = Modifier.height(50.dp))
+        Box(
+            contentAlignment = Alignment.TopStart,
+            modifier = Modifier
+                .background(color = Color(0xFFe3fcff))
+                .border(width = 2.dp, color = Color.Black)
+                .padding(15.dp)
+                .width(300.dp)
+                .height(50.dp)
+        ) {
+            Column(horizontalAlignment = Alignment.Start) {
+                Text("Disc Golf", fontSize = (18.sp))
+                Spacer(modifier = Modifier.height(10.dp))
+                Row(horizontalArrangement = Arrangement.SpaceAround) {
+                    Text("Brahm Rifino ", fontSize = (14.sp), fontWeight = FontWeight.Bold)
+                    Text("(CS 492)", fontSize = (14.sp))
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(30.dp))
+        Row( modifier = Modifier.padding(bottom = 16.dp), horizontalArrangement = Arrangement.SpaceAround) {
             Button(
                 onClick = {
-                    count -= 1
-                    if(count < 0){
-                        count = 0
-                    }}
+                    if (count == 1) count = 5 else count--
+                }
             ) {
                 Text(text = "Previous", fontSize = 24.sp)
             }
-            Spacer(modifier = Modifier.width(90.dp))
+            Spacer(modifier = Modifier.width(45.dp))
             Button(
                 onClick = {
-                    count += 1
-                    if (count > 5) {
-                        count = 1
-                    }
+                    if (count == 5) count = 1 else count++
                 },
-                modifier = Modifier.padding(end = 16.dp)
+                modifier = Modifier.width(150.dp)
             ) {
                 Text(text = "Next", fontSize = 24.sp)
             }
